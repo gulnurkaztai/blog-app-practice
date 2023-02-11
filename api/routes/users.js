88a90c9b149c2520704c8ad3d5c32ef315134412
gulnurkaztai <kaztai.gulnur@gmail.com>
2023-02-11 17:14:20 -0500

    finish users router

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const User  = require("../models/User")
+const Post  = require("../models/Post")
 const bcrypt =require("bcrypt")
 
 //Update
@@ -28,6 +29,38 @@ router.put("/:id", async (req, res) => {
   });
 
   // Delete
+  router.delete("/:id", async (req, res) => {
+    if (req.body.userId === req.params.id) {
+      try{
+        const user = await User.findById(req.params.id);
+        try {
+          await Post.deleteMany({username: req.body.username})
+          await User.findByIdAndDelete(req.params.id);
+          res.status(200).json("User has been deleted");
+        } catch (err) {
+          res.status(500).json(err);
+        }
+
+      } catch(err){
+        res.status(404).json("User Not Found")
+      }
+
+    } else {
+      res.status(401).json("You can delete only your account!");
+    }
+  });
+
+  //Get user
+
+  router.get("/:id", async(req,res)=>{
+    try{
+      const user = await User.findById(req.params.id)
+      const {password, ...others}= user._doc
+      res.status(200).json(others);
+    }catch(err){
+      res.status(500).json(err)
+    }
+  })
 
   
 
