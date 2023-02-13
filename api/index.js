86663eb928ce1329137ecfebbbbd8b3ead50ad87
gulnurkaztai <kaztai.gulnur@gmail.com>
2023-02-12 21:35:22 -0500

    category route

--- api/index.js
+++ api/index.js
@@ -6,6 +6,7 @@ mongoose.set('strictQuery', true);
 const authRoute = require("./routes/auth")
 const userRoute = require("./routes/users")
 const postRoute = require("./routes/posts")
+const categoryRoute = require("./routes/categories")
 
 app.use(express.json());
 mongoose.connect(process.env.MONGO_URI)
@@ -15,6 +16,7 @@ mongoose.connect(process.env.MONGO_URI)
 app.use("/api/auth", authRoute)
 app.use("/api/users", userRoute)
 app.use("/api/posts", postRoute)
+app.use("/api/categories", categoryRoute)
 
 app.listen(5001, ()=>{
     console.log("Listeninggg")
---
+++ api/routes/categories.js
@@ -0,0 +1,27 @@
+const router = require("express").Router();
+const Category  = require("../models/Category")
+
+
+//Create a category
+router.post("/", async(req,res)=>{
+    const newCategory = new Category(req.body);
+    try{
+        const savedCategory = await newCategory.save()
+        res.status(200).json(savedCategory)
+    }catch(err){
+        res.status(500).json(err)
+    }
+})
+
+//Get all categories
+router.get("/", async(req,res)=>{
+    try{
+        const allCategories = await Category.find()
+        res.status(200).json(allCategories)
+    } catch(err){
+        res.status(500).json(err)
+    }
+})
+
+
+module.exports = router;
