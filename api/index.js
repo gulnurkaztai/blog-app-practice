4df61bccf86f9acaae60931c022f69aeefc6703d
gulnurkaztai <kaztai.gulnur@gmail.com>
2023-02-14 10:22:12 -0500

    debugged img upload

--- api/index.js
+++ api/index.js
@@ -8,8 +8,12 @@ const userRoute = require("./routes/users")
 const postRoute = require("./routes/posts")
 const categoryRoute = require("./routes/categories")
 const multer = require("multer")
+const path = require("path")
 
 app.use(express.json());
+app.use("/images", express.static(path.join(__dirname, "/images")))
+
+
 mongoose.connect(process.env.MONGO_URI)
 .then(console.log("connected to MongoDb"))
 .catch((err) =>console.log(err));
